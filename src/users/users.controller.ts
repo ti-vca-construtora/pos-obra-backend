0d9d73985e8426cbfe9f912e03e927ee484dc735
Jorge Jesus Santos <jorge.santos@vcaconstrutora.com.br>
2026-01-22 09:06:55 -0300

    alteração no user e na documentação

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('users')
@@ -13,7 +14,7 @@ constructor(private readonly usersService: UsersService) {}
 @Roles('ADMIN')
 @Get()
 findAll(){
-    return 'Somente ADMIN';
+    return this.usersService.findAll();    
 }
 
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -27,6 +28,20 @@ create(@Body() dto: CreateUserDto) {
 @Get('me')
 getProfile(){
     return 'admin ou user';
+}
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('ADMIN')
+@Patch(':id')
+update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+  return this.usersService.update(+id, dto);
+}
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('ADMIN')
+@Delete(':id')
+remove(@Param('id') id: string) {
+  return this.usersService.remove(+id);
 }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,39 +1,87 @@
-import { Injectable } from '@nestjs/common';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-
 import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-
   constructor(private prisma: PrismaService) {}
 
   async create(dto: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(dto.password,10);
+    const exists = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (exists) {
+      throw new BadRequestException('Email já cadastrado');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
 
     return this.prisma.user.create({
       data: {
-        email:dto.email,
-        nome:dto.nome,
-        password:hashedPassword,
-        role: dto.role,
+        email: dto.email,
+        nome: dto.nome,
+        password: hashedPassword,
+        role: dto.role ?? 'USER',
         active: true,
       },
-      select:{
-        id:true,
-        email:true,
-        nome:true,
-        role:true,
-        active:true,
-        createdAt:true,
+      select: {
+        id: true,
+        email: true,
+        nome: true,
+        role: true,
+        active: true,
+        createdAt: true,
       },
     });
   }
 
   findAll() {
-    return this.prisma.user.findMany();
-  } 
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        nome: true,
+        role: true,
+        active: true,
+        createdAt: true,
+      },
+    });
+  }
+
+
+async update(id: number, dto: UpdateUserDto) {
+  const data: any = { ...dto };
+
+  if (dto.password) {
+    data.password = await bcrypt.hash(dto.password, 10);
+  }
+
+  return this.prisma.user.update({
+    where: { id },
+    data,
+    select: {
+      id: true,
+      email: true,
+      nome: true,
+      role: true,
+      active: true,
+    },
+  });
+}
+
+async remove(id: number) {
+  return this.prisma.user.update({
+    where: { id },
+    data: {
+      active: false,
+    },
+  });
+}
+
 
 
 
