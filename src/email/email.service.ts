35937e4a756f403136d9034bc4f2c004c4815ee1
jsmaximo <jorgejs2013@gmail.com>
2026-03-04 11:18:16 -0300

    atualizacao email

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -13,80 +13,76 @@ interface EnviarEmailParams {
 
 @Injectable()
 export class EmailService {
-  private transporter: nodemailer.Transporter;
+
+  private resend: Resend;
 
   constructor(private configService: ConfigService) {
-    this.transporter = nodemailer.createTransport({
-      host: this.configService.get<string>('SMTP_HOST'),
-      port: Number(this.configService.get<number>('SMTP_PORT')),
-      secure:
-        Number(this.configService.get<number>('SMTP_PORT')) === 465,
-      auth: {
-        user: this.configService.get<string>('SMTP_USER'),
-        pass: this.configService.get<string>('SMTP_PASS'),
-      },
-    });
-  }
-
-  private carregarTemplate(
-  nomeTemplate: string,
-  variaveis: Record<string, string | number> = {},
-): string {
-
-  const caminhoDev = path.join(
-    process.cwd(),
-    'src',
-    'email',
-    'templates',
-    `${nomeTemplate}.html`,
-  );
-
-  const caminhoProd = path.join(
-    __dirname,
-    'templates',
-    `${nomeTemplate}.html`,
-  );
-
-  let caminhoFinal = '';
-
-  if (fs.existsSync(caminhoDev)) {
-    caminhoFinal = caminhoDev;
-  } else if (fs.existsSync(caminhoProd)) {
-    caminhoFinal = caminhoProd;
-  } else {
-    throw new Error(`Template ${nomeTemplate} não encontrado`);
-  }
-
-  let html = fs.readFileSync(caminhoFinal, 'utf-8');
-
-  for (const chave in variaveis) {
-    html = html.replace(
-      new RegExp(`{{${chave}}}`, 'g'),
-      String(variaveis[chave]),
+    this.resend = new Resend(
+      this.configService.get<string>('RESEND_API_KEY'),
     );
   }
 
-  return html;
-}
+  private carregarTemplate(
+    nomeTemplate: string,
+    variaveis: Record<string, string | number> = {},
+  ): string {
 
+    const caminhoDev = path.join(
+      process.cwd(),
+      'src',
+      'email',
+      'templates',
+      `${nomeTemplate}.html`,
+    );
+
+    const caminhoProd = path.join(
+      __dirname,
+      'templates',
+      `${nomeTemplate}.html`,
+    );
+
+    let caminhoFinal = '';
+
+    if (fs.existsSync(caminhoDev)) {
+      caminhoFinal = caminhoDev;
+    } else if (fs.existsSync(caminhoProd)) {
+      caminhoFinal = caminhoProd;
+    } else {
+      throw new Error(`Template ${nomeTemplate} não encontrado`);
+    }
+
+    let html = fs.readFileSync(caminhoFinal, 'utf-8');
+
+    for (const chave in variaveis) {
+      html = html.replace(
+        new RegExp(`{{${chave}}}`, 'g'),
+        String(variaveis[chave]),
+      );
+    }
+
+    return html;
+  }
 
   async enviar(params: EnviarEmailParams) {
     try {
+
       const html = this.carregarTemplate(
         params.template,
         params.variaveis,
       );
 
-      await this.transporter.sendMail({
-        from: `"Pós Obra" <${this.configService.get<string>('SMTP_FROM')}>`,
+      await this.resend.emails.send({
+        from: `Pós Obra <${this.configService.get<string>('EMAIL_FROM')}>`,
         to: params.para,
         subject: params.assunto,
-        html,
+        html: html,
       });
 
       return true;
+
     } catch (error) {
       console.error('ERRO AO ENVIAR EMAIL >>>', error);
+
       throw new InternalServerErrorException(
         'Erro ao enviar email',
       );
